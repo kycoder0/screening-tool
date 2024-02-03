de1b721e5a8d4dd39239fc236ebae1d6019e603c
Trevor Rice <42627940+kycoder0@users.noreply.github.com>
2024-02-03 10:05:28 -0500

    feat: add input_type to question model

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Question extends Model
 {
     use HasFactory;
-    protected $fillable = ['form_id', 'name', 'text', 'validation_rules', 'visibility_rule'];
+    protected $fillable = ['form_id', 'name', 'text', 'input_type', 'validation_rules', 'visibility_rule'];
 
     protected $casts = [
         'validation_rules' => 'array',
