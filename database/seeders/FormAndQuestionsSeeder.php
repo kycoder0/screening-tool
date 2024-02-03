e88fd4a219e4a9a7df7b313661ddc5cbf0124741
Trevor Rice <42627940+kycoder0@users.noreply.github.com>
2024-02-03 11:31:22 -0500

    feat: add label to question

--- database/seeders/FormAndQuestionsSeeder.php
+++ database/seeders/FormAndQuestionsSeeder.php
@@ -30,6 +30,7 @@ class FormAndQuestionsSeeder extends Seeder
         $questions = [
             [
                 'name' => 'first_name',
+                'label' => 'First Name',
                 'text' => 'What is your first name?',
                 'input_type' => 'text',
                 'validation_rules' => ['required', 'string', 'max:255'],
@@ -38,6 +39,7 @@ class FormAndQuestionsSeeder extends Seeder
             [
                 'name' => 'date_of_birth',
                 'text' => 'What is your date of birth?',
+                'label' => 'Date of Birth',
                 'input_type' => 'date',
                 'validation_rules' => ['required', 'date'],
                 'visibility_rule' => 'always_visible',
@@ -45,6 +47,7 @@ class FormAndQuestionsSeeder extends Seeder
             [
                 'name' => 'migraine_frequency',
                 'text' => 'How often do you experience migraines?',
+                'label' => 'Migraine Frequency',
                 'input_type' => 'radio',
                 'options' => ['daily', 'weekly', 'monthly'],
                 'validation_rules' => ['required'],
@@ -53,6 +56,7 @@ class FormAndQuestionsSeeder extends Seeder
             [
                 'name' => 'daily_frequency',
                 'text' => 'How many times a day do you experience migraines?',
+                'label' => 'Daily Frequency',
                 'input_type' => 'radio',
                 'options' => ['1-2', '3-4', '5+'],
                 'validation_rules' => ['required'],
