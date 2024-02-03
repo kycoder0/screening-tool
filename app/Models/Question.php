03c79b654c9df66505f1afea631e7fe4076ac368
Trevor Rice <42627940+kycoder0@users.noreply.github.com>
2024-02-03 10:11:19 -0500

    feat: add nullable options to question model

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -9,11 +9,12 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Question extends Model
 {
     use HasFactory;
-    protected $fillable = ['form_id', 'name', 'text', 'input_type', 'validation_rules', 'visibility_rule'];
+    protected $fillable = ['form_id', 'name', 'text', 'input_type', 'options', 'validation_rules', 'visibility_rule'];
 
     protected $casts = [
         'validation_rules' => 'array',
         'visibility_rule' => 'array',
+        'options' => 'array',
     ];
 
     public function form(): BelongsTo
