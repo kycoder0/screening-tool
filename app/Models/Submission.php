5ffdf77020596ebf28806a44ee7d4f385bc7cafa
Trevor Rice <42627940+kycoder0@users.noreply.github.com>
2024-02-04 09:10:14 -0500

    fix: change hasOne to belongsTo in submission

--- app/Models/Submission.php
+++ app/Models/Submission.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Submission extends Model
@@ -12,8 +13,13 @@ class Submission extends Model
 
     protected $fillable = ['name', 'answers', 'form_id', 'outcome_id', 'user_ip'];
 
-    public function outcome(): HasOne
+    public function outcome(): BelongsTo
     {
-        return $this->hasOne(Outcome::class);
+        return $this->belongsTo(Outcome::class);
+    }
+
+    public function form(): BelongsTo
+    {
+        return $this->belongsTo(Form::class);
     }
 }
